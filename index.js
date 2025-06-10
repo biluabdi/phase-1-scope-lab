dcdc72772f42f87e1fef336b54efba37a72f544f
bilu abdi <biluabdi91@gmail.com>
2025-06-10 07:56:26 +0300

    completed scopelab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// 1. Declare a global variable using var
+var customerName = 'bob';
+
+// 2. Function to uppercase the global customerName
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// 3. Function to declare bestCustomer in global scope
+function setBestCustomer() {
+  bestCustomer = 'not bob'; // implicitly global (bad practice!)
+}
+
+// 4. Function to overwrite the global bestCustomer variable
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+// 5. Declare a constant in global scope
+const leastFavoriteCustomer = 'Karen';
+
+// 6. Function that tries to change a constant (this will throw an error)
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // TypeError
+}
+
